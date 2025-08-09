bb4b8f98791b2b4e8e96f3fb26f4b3275d14f916
HavenGoitom <havengoitom2@gmail.com>
2025-08-09 08:53:54 +0300

    .

--- Backend/Journal/settings.py
+++ Backend/Journal/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() in ('true', '1', 't')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
